69cdec5f3435ff6a94adb86f8652cf35de7fb77f
2gingercats12345 <pflp4student@KSPFL-Y406.local>
2016-06-07 16:22:28 +0800

    game!

---
+++ game/js/index.js
@@ -0,0 +1,30 @@
+function playGame () {
+
+var question1=prompt( "How many metres is the longest snake in the world,14,20,13,16?")
+
+
+if(question1==="14") {
+ alert('you are correct!');
+}
+else{alert('You are WRONG!!!')};
+
+var question2=prompt('There are loads of different snakes which is the most DANGROUS!!!????? The black mamba,the garden snake or a rattle snake?');
+
+if(question2==='black mamba'){alert('You are CORRECT!!!!!!!')} else{alert('YOU ARE WRONG!!!')};
+
+var question3=prompt('Where does snake venom come from?The head,the body or the tummy') 
+
+if(question3==='the head'){alert('Yes belive it or not snakes get venom from their heads!!! TRUE FACT😀')}
+  
+else{alert('YOU ARE WRONG!!!')};
+var question4=prompt('What is the fattest breed of snake in the WORLD!??!!?!?!?!?!?!??!!?!?!?!?!?!?!? the anacconda the python the black mamba?') 
+if(question4==='anacconda'){alert('Yes  the anacconda breed is the FATTEST IN THE WORLD TRUE FACT😀')}else{alert('WRONG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')}
+
+var question5=prompt('What is the smallest snake in the world?Leptotyphlops carlae,garden snake,python!!!!!!!????????????!!!!!!!!!!!!!!!!!!!!!!!????????????????????????????') 
+
+if(question5==='Leptotyphlops carlae'){alert('Yes the Leptotyphlops carlae  breed is the TINIEST IN THE WORLD this snake is 20mm long TRUE FACT')}else{alert('WRONG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! The Leptotyphlops carlae is the smallest only 20mm WOW!!!!!!!!🐍')}var 
+
+question6=prompt(' What is the most  poisonous snake in the world ??????????Inland taipan,chicken nuggets,chocolate cake,oh sorry,errrrmmmmmm,python,anacconda or tiger snake???') 
+
+if(question6==='Inland taipan'){alert('Yes the Inland taipan breed is the poisonous snake IN THE WORLD this snake can kill 1000 people!!!!!!!!  FACT')}else{alert('WRONG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! The Inland taipan is the most toxic!🐍')}
+}
